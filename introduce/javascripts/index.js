364a232e6963eb940c4c1af3e89fcb2117f2e329
majinmaru <gotch11@naver.com>
2019-04-24 13:22:08 +0900

    190424

--- introduce/javascripts/index.js
+++ introduce/javascripts/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	
+	$(".img-me-box img").addClass('up');
+	function squareIcon(){
+		$(".indicator-line").addClass('roll');
+		setTimeout(function(){
+			$(".indicator-line").addClass('line');
+		}, 500);
+		setTimeout(function(){
+			$(".indicator-text").addClass('appear');
+		}, 1000);
+	}
+	squareIcon();
 });
