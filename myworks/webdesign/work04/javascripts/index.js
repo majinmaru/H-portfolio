829f4167f7dc3965597dde26105c9eeb93b13430
majinmaru <gotch11@naver.com>
2019-04-29 23:04:25 +0900

    190429

---
+++ myworks/webdesign/work04/javascripts/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	function squareIcon(){
+		$(".indicator-line").addClass('roll');
+		setTimeout(function(){
+			$(".indicator-line").addClass('line');
+		}, 500);
+		setTimeout(function(){
+			$(".indicator-text").addClass('appear');
+		}, 1000);
+	}
+	squareIcon();
+	function jumboImg(){
+		$(".jumbo-img").addClass('expand');
+	}
+	jumboImg();
+
+	function scrollIcon(){
+		$(".scroll-icon").addClass('active');
+	}
+	setTimeout(function(){
+		scrollIcon();
+	}, 500);
+});
