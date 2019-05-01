23ac59e06b9f322a4598437623cede2299a1992e
majinmaru <gotch11@naver.com>
2019-05-02 00:42:54 +0900

    190502

--- contact/javascripts/index.js
+++ contact/javascripts/index.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-	
+	function squareIcon(){
+		$(".indicator-line").addClass('roll');
+		setTimeout(function(){
+			$(".indicator-line").addClass('line');
+		}, 500);
+		setTimeout(function(){
+			$(".indicator-text").addClass('appear');
+		}, 1000);
+	}
+	squareIcon();
 });
