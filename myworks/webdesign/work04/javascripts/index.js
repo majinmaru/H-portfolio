e9571be5914f91adc2dd8c450e1fbf75dac800f9
majinmaru <gotch11@naver.com>
2019-05-01 15:05:07 +0900

    190501
    gg

--- myworks/webdesign/work04/javascripts/index.js
+++ myworks/webdesign/work04/javascripts/index.js
@@ -23,4 +23,15 @@ $(document).ready(function(){
 	setTimeout(function(){
 		scrollIcon();
 	}, 2000);
+	
+	AOS.init({
+   		duration: 1000
+   	});
+
+   	$(".convenient-context__img").find("img").paroller({
+   		factor: 0.1, 
+   		type: 'foreground',     // background, foreground
+        direction: 'vertical', // vertical, horizontal
+        transition: 'transform 0.5s ease' // CSS transition, added on elements where type:'foreground'
+   	});
 });
